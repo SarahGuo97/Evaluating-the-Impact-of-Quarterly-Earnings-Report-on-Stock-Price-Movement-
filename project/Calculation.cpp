0fec563d7025960520e53e45862f1ca4c676ee48
SarahGuo <54755617+SarahGuo97@users.noreply.github.com>
2020-07-03 11:20:58 -0400

    Update Calculation.cpp

--- project/Calculation.cpp
+++ project/Calculation.cpp
@@ -1,11 +1,3 @@
-//
-//  simple_crul.cpp
-//  final_project
-//
-//  Created by 张嘉昕 on 5/1/20.
-//  Copyright © 2020 NYU. All rights reserved.
-//
-
 #include "Calculation.h"
 using namespace std;
 
